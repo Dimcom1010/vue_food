d7c15fd3665a0533217ac0423eff7bfe1e63a0da
Dimcom1010 <dimcom1010@gmail.com>
2024-12-02 22:56:26 +0300

    [add] добавлен сервис для получения данных

--- src/components/BodyComponent/BodyComponent.vue
+++ src/components/BodyComponent/BodyComponent.vue
@@ -1,4 +1,31 @@
+<template>
+  <div class="body-component">
+    <h1>Body Component</h1>
+    <div v-if="loading">Loading...</div>
+    <div v-else-if="error">{{ error }}</div>
+    <div v-else>
+      <pre>{{ data }}</pre>
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
-import styles from "./BodyComponent.module.css";
+import { ref, onMounted } from 'vue';
+import { fetchData } from '@/services/apiService';
+import styles from './BodyComponent.module.css';
+
+const data = ref(null);
+const loading = ref(false);
+const error = ref('');
+
+onMounted(async () => {
+  try {
+    loading.value = true;
+    data.value = await fetchData();
+  } catch (err) {
+    error.value = 'Ошибка при получении данных';
+  } finally {
+    loading.value = false;
+  }
+});
 </script>
-<template>Body Component</template>
