264385edb2d1962f79fc35dbab50c7f3fc87cb4b
Dimcom1010 <dimcom1010@gmail.com>
2024-12-02 21:17:50 +0300

    [add] добавлена папка pages сомпонент App перенес\т в соответствующую папку и разбит на стили и шаблон

---
+++ src/pages/app/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import HelloWorld from '../../components/HelloWorld.vue'
+import TheWelcome from '../../components/TheWelcome.vue'
+import styles from './App.module.css'
+</script>
+
+<template>
+  <header :class="styles.header">
+    <img alt="Vue logo" :class="styles.logo" src="../../assets/logo.svg" width="125" height="125" />
+
+    <div :class="styles.wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
