350729961b8d9a10329aa8a15695bc7a641a0a83
Dimcom1010 <dimcom1010@gmail.com>
2024-12-03 22:00:45 +0300

    [add] Добавление Footer

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import styles from "./Footer.module.less";
+</script>
+
+<template>
+    <div :class="styles.footer">
+        <div :class="styles.footer__title">Витебск 2024</div>
+        <div :class="styles.footer__body">
+            <a href="https://github.com/Dimcom1010/vue_food/blob/main/README.md"
+                >ссылка на README</a
+            >
+        </div>
+    </div>
+</template>
